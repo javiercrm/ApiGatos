8a18b8093419592500f2fd6669f4185bb8b708e5
javiercrm <43179062+javiercrm@users.noreply.github.com>
2019-02-12 18:30:21 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,29 +1,172 @@
 //Url de donde cogemos los 50 gatos
-var urlGatos = "https://api.thecatapi.com/v1/images/search?api_key=44cffd46-cdad-4e2a-85b0-07ccb3e0d953&limit=50&category_ids=";
+var urlGatos;
 
 //Url de donde cojemos las categorias
 var urlCategorias = "https://my-json-server.typicode.com/javiercrm/CategoriasGatos/Categorias/";
 
 //Pagina actual
-var page = 1;
+var page;
 
-//Categoria Actual
-var categoria = document.getElementById('opciones').value;
-
-//Total de imagenes por pagina
-var totalPerPage = document.getElementById('tamPagina').value;
-
-//Primera imagen a mostrar
-var inicio = (page-1) * totalPerPage;
+//Categoria
+var Categoria;
 
 //Numero total de paginas
-var totalPage = Math.ceil(50 / totalPerPage);
+var totalPage;
+
+//Fotos por paginas
+var totalPerPage;
 
 //Evento onchange de los dos select
 document.getElementById('SeleccionCategoria').onchange = cambiarCategoria;
 document.getElementById('tamPagina').onchange = cambiarTamPorPagina;
 
-//Añadimos la paginacion
+
+//ObtenerTotalGatos
+function totalGatos() {
+    obtenerDatos("https://api.thecatapi.com/v1/images/search?limit=50&category_ids="+categoria+"&order=Desc&api_key=22216bca-bbcc-4a82-aa98-39dc74e7a458").then(function(array) {
+    
+        mostrarGatos();
+        anadirPagination();
+        eventoClick();
+        
+    }, function(status) {
+    alert('Algo fue mal.');
+    });
+}
+
+//Obtenemos datos mediante una promesa
+function obtenerDatos(url) {
+    return new Promise(function(resolve, reject) {
+
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = function() {
+
+            if (xhr.status == 200) {
+                resolve(xhr.response);
+                if (url.indexOf('api.thecatapi.com') != -1) {
+                    if(xhr.getResponseHeader("Pagination-Count") > 0){
+                            if(xhr.getResponseHeader("Pagination-Count") > 50){
+                                totalPage = Math.ceil((50 / totalPerPage));
+                            } else {
+                                totalPage = Math.ceil(((xhr.getResponseHeader("Pagination-Count")) / totalPerPage));
+                            }
+                    } else {
+                        totalPage = 0;
+                    }
+                }
+                
+            } else {
+                reject(xhr.status);
+            }
+        };
+
+        xhr.send();
+
+    });
+}
+
+function updateUrl(totalPerPages, pages, categorias) {
+    let aux = totalPerPages;
+    if (totalPage === pages) {
+        totalPerPages = parseInt((50 - (totalPerPages*(pages-1))));
+    }
+    if (totalPerPages > totalPerPage) {
+        totalPerPages = totalPerPage;
+    }
+    urlGatos = "https://api.thecatapi.com/v1/images/search?limit="+totalPerPages+"&page=" + (pages-1) + "&category_ids="+categorias+"&order=Desc&api_key=22216bca-bbcc-4a82-aa98-39dc74e7a458";
+    page = parseInt(pages);
+    totalPerPage = parseInt(aux);
+    categoria = parseInt(categorias);
+}
+
+function cambiarCategoria() {
+    categoria = document.getElementById('opciones').value;
+    updateUrl(totalPerPage, 1, categoria);
+    limpiarFotos();
+    limpiarPagination();
+    totalGatos();
+}
+
+//Cambiar el total de imagenes por pagina
+function cambiarTamPorPagina() {
+    totalPerPage = document.getElementById('tamPagina').value;
+    updateUrl(totalPerPage, 1, categoria);
+    limpiarFotos();
+    limpiarPagination();
+    totalGatos();
+}
+
+function limpiarFotos() {
+    if (document.getElementById('photos') != null) {
+        document.getElementById('photos').remove(); 
+     }
+}
+
+function limpiarPagination() {
+    if (document.getElementById('center') != null) {
+       document.getElementById('center').remove();
+    }
+    
+}
+
+//Mostramos el select de categorias
+function mostrarCategorias(){
+    obtenerDatos(urlCategorias).then(function(array) {
+        array.forEach(function (currentValue, index, array) {
+            let despegable = document.getElementById('opciones');
+            let opcion = document.createElement('OPTION');
+
+            opcion.value = currentValue.id;
+            opcion.textContent = currentValue.name;
+
+            despegable.appendChild(opcion);
+        });        
+        
+    }, function(status) {
+    alert('Algo fue mal.');
+    });
+}
+
+//Mostarmos las imagenes
+function mostrarGatos() {
+
+    obtenerDatos(urlGatos).then(function(array) {
+
+        document.getElementById('titlePaginas').textContent = page + " / " + totalPage;
+
+        if (totalPage == 0) {
+            limpiarPagination();
+            let div = document.getElementById('photos');
+            let error = document.createElement('H1');
+            error.textContent = 'No Se han encontrado Imagenes en esta categoria';
+            div.appendChild(error);
+        } else {
+            array.forEach(function(currentValue, index, array){
+                
+                    let div = document.getElementById('photos');
+                    let li = document.createElement('LI');
+                    let img = document.createElement('IMG');
+        
+                    li.setAttribute('class', 'grid-25 tablet-grid-50');
+        
+                    img.setAttribute('class', 'image');
+                    img.setAttribute('src', currentValue.url);
+        
+                    li.appendChild(img);
+                    div.appendChild(li);
+        
+            });
+        }
+        
+        
+    }, function(status) {
+    alert('Algo fue mal.');
+    });
+    
+}
+
 function anadirPagination() {
 
     let caja = document.getElementById('contained');
@@ -42,14 +185,14 @@ function anadirPagination() {
 
     cajaPadre.appendChild(divCentrado);
 
-    TotalPaginas.textContent = page + " / " + totalPage;
+    //TotalPaginas.textContent = page + " / " + totalPage;
     TotalPaginas.id = 'titlePaginas';
     divCentrado.appendChild(TotalPaginas);
 
     divCentrado.appendChild(divPagination);
 
 
-    if (page == 1) {
+    if (page === 1) {
 
         for (let i = 0; i < 3; i++) {
             
@@ -67,8 +210,14 @@ function anadirPagination() {
         button.id = (page+1);
         button.textContent = '>';
         divPagination.appendChild(button);
+
+        let buttondos = document.createElement('BUTTON');
+        buttondos.id = totalPage;
+        buttondos.textContent = '>>';
+        divPagination.appendChild(buttondos);
+
         
-    } else if (page == totalPage) {
+    } else if (page === totalPage) {
 
         let pos = totalPage - 2;
 
@@ -136,131 +285,10 @@ function anadirPagination() {
     
 }
 
-//Quitamos la caja de paginacion
-function limpiarPagination() {
-    if (document.getElementById('center') != null) {
-       document.getElementById('center').remove();
-    }
-    
-}
-
-//Quitamos las fotos de la pagina
-function limpiarFotos() {
-    if (document.getElementById('photos') != null) {
-        document.getElementById('photos').remove(); 
-     }
-}
-
-//Cambiamos la categoria
-function cambiarCategoria() {
-    page = 1;
-    inicio = (page-1) * totalPerPage;
-    categoria = document.getElementById('opciones').value;
-    limpiarFotos();
-    limpiarPagination();
-    mostrarGatos();
-    anadirPagination();
-    eventoClick();
-}
-
-//Cambiar el total de imagenes por pagina
-function cambiarTamPorPagina() {
-    page = 1;
-    totalPerPage = document.getElementById('tamPagina').value;
-    totalPage = Math.ceil(50 / totalPerPage);
-    limpiarFotos();
-    limpiarPagination();
-    mostrarGatos();
-    anadirPagination();
-    eventoClick();
-}
-
-//Obtenemos datos mediante una promesa
-function obtenerDatos(url) {
-    return new Promise(function(resolve, reject) {
-
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'json';
-        xhr.onload = function() {
-
-            if (xhr.status == 200) {
-                resolve(xhr.response);
-            } else {
-                reject(xhr.status);
-            }
-        };
-
-        xhr.send();
-
-    });
-}
-
-//Mostramos el select de categorias
-function mostrarCategorias(){
-    obtenerDatos(urlCategorias).then(function(array) {
-        array.forEach(function (currentValue, index, array) {
-            let despegable = document.getElementById('opciones');
-            let opcion = document.createElement('OPTION');
-
-            opcion.value = currentValue.id;
-            opcion.textContent = currentValue.name;
-
-            despegable.appendChild(opcion);
-        });        
-        
-    }, function(status) {
-    alert('Algo fue mal.');
-    });
-}
-
-//Mostarmos las imagenes
-function mostrarGatos() {
-
-    obtenerDatos(urlGatos+categoria).then(function(array) {
-
-        totalPage = Math.ceil(array.length / totalPerPage);
-        document.getElementById('titlePaginas').textContent = page + " / " + Math.ceil(array.length / totalPerPage);
-
-        if (array.length == 0) {
-            limpiarPagination();
-            let div = document.getElementById('photos');
-            let error = document.createElement('H1');
-            error.textContent = 'No Se han encontrado Imagenes en esta categoria';
-            div.appendChild(error);
-        } else {
-            document.getElementById('titlePaginas').textContent = page + " / " + Math.ceil(array.length / totalPerPage);
-            array.forEach(function(currentValue, index, array){
-    
-                if (parseInt(index) >= parseInt(inicio) && parseInt(index) < parseInt(inicio)+parseInt(totalPerPage)) {
-                    let div = document.getElementById('photos');
-                    let li = document.createElement('LI');
-                    let img = document.createElement('IMG');
-        
-                    li.setAttribute('class', 'grid-25 tablet-grid-50');
-        
-                    img.setAttribute('class', 'image');
-                    img.setAttribute('src', currentValue.url);
-        
-                    li.appendChild(img);
-                    div.appendChild(li);
-                }
-        
-            });
-        }
-        
-        
-    }, function(status) {
-    alert('Algo fue mal.');
-    });
-    
-}
-
 //Evento al hacer click en la paginacion
 function eventoClick(){
     document.getElementById('pagination').addEventListener('click', (event) =>{
-        page = parseInt(event.target.id);
-        inicio = (page-1) * totalPerPage;
+        updateUrl(totalPerPage, parseInt(event.target.id), categoria);
         limpiarPagination();
         limpiarFotos();
         mostrarGatos();
@@ -270,8 +298,6 @@ function eventoClick(){
     });
 }
 
-//Iniciamos la muesta de Imagenes, paginacion y categorias al iniciar la pagina
+updateUrl(5, 1, 5);
 mostrarCategorias();
-mostrarGatos();
-anadirPagination();
-eventoClick();
+totalGatos();
